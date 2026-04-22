c69d0ee1d073f16a609f8b62902ad59c791a59dc
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:26 -0500

    TUnit conversion

--- test/Soenneker.Plex.Runners.OpenApiClient.Tests/PlexOpenApiClientRunnerTests.cs
+++ test/Soenneker.Plex.Runners.OpenApiClient.Tests/PlexOpenApiClientRunnerTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Plex.Runners.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public sealed class PlexOpenApiClientRunnerTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class PlexOpenApiClientRunnerTests : HostedUnitTest
 {
-    public PlexOpenApiClientRunnerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public PlexOpenApiClientRunnerTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
