ed2c5a354a89b0a621a6bdae5ef93f8d025e3eba
soenneker <jake@soenneker.com>
2026-09-03 22:01:38 -0500

    Add interface inheritdoc references

--- src/Soenneker.Plex.Runners.OpenApiClient/Utils/FileOperationsUtil.cs
+++ src/Soenneker.Plex.Runners.OpenApiClient/Utils/FileOperationsUtil.cs
@@ -20,6 +20,7 @@ using System.Collections.Generic;
 
 namespace Soenneker.Plex.Runners.OpenApiClient.Utils;
 
+/// <inheritdoc cref="IFileOperationsUtil" />
 public sealed class FileOperationsUtil : IFileOperationsUtil
 {
     private readonly ILogger<FileOperationsUtil> _logger;
